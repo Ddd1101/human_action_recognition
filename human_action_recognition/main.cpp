0b9e02c7cc2585e1ebb28e58e386c912612e11e6
DD D <752042257@qq.com>
2018-04-20 21:10:28 +0800

    part1

--- human_action_recognition/main.cpp
+++ human_action_recognition/main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 typedef vector<Point> contour_t;
 
-void lsn5_Foreground(void);
+void ContrastAndBright(Mat &src, Mat &dst, double alpha, double beta);//增强对比度和亮度  alpha对比度 beta亮度
 
 int main() {
 	//视频路径
@@ -31,7 +31,7 @@ int main() {
 	double rate = capture.get(CV_CAP_PROP_FPS);
 	int delay = 1000 / rate;
 
-	Mat frame;
+	Mat src, frame;
 	Mat foreground;
 	Mat mask, f1, f2, f3, f4;
 	Mat fgimg;
@@ -42,10 +42,17 @@ int main() {
 	bgsubstractor->setVarThreshold(20);
 	bool stop(false);
 	while (!stop) {
-		if (!capture.read(frame))
+		if (!capture.read(src))
 		{
 			break;
 		}
+
+		//增加对比度
+		//frame = src;
+		frame = Mat::zeros(src.size(), src.type());
+		ContrastAndBright(src, frame, 1.5, 50);
+
+		//图像大小变化
 		//resize(frame, frame, Size(frame.cols * 2, frame.rows * 2), 0, 0, INTER_LINEAR);
 
 		if (fgimg.empty())
@@ -65,7 +72,7 @@ int main() {
 		//GaussianBlur(mask, mask, Size(5, 5), 3.5, 3.5);//高斯平滑
 		it++;
 
-		//切割人体图像
+		///////////////////////////////////////切割人体图像
 		//===== 对前景掩码做处理，过滤噪点
 		vector<vector<Point>> contours;
 		Mat contours_src = f1.clone();
@@ -83,7 +90,6 @@ int main() {
 		//对前景掩码的再次掩码处理
 		Mat fgmask1;
 		f1.copyTo(fgmask1, mask2);
-		//imshow("mask2", mask2);
 		//===== 前景掩码处理完毕
 
 		//获取前景图像
@@ -92,7 +98,7 @@ int main() {
 
 		imshow("foreground", fgimg);
 		//imshow("mask", f1);
-		imshow("src",frame);
+		imshow("src", frame);
 		waitKey(100);
 		/*if (waitKey(delay) > 0)
 		stop = true;*/
@@ -101,4 +107,18 @@ int main() {
 	waitKey();
 	return 0;
 
+}
+
+void ContrastAndBright(Mat &src, Mat &dst, double alpha, double beta) {
+	// 执行变换 new_image(i,j) = alpha    * image(i,j) + beta
+	for (int y = 0; y < src.rows; y++)
+	{
+		for (int x = 0; x < src.cols; x++)
+		{
+			for (int c = 0; c < 3; c++)
+			{
+				dst.at<Vec3b>(y, x)[c] = saturate_cast<uchar>(alpha * (src.at<Vec3b>(y, x)[c]) + beta);
+			}
+		}
+	}
 }
